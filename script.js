9ac1a811eda4a919dc2f43cd7b12518f29a38d31
Dragonterror <144467487+DragonterrorOfficial@users.noreply.github.com>
2024-08-27 07:39:06 -0400

    test

---
+++ script.js
@@ -0,0 +1,41 @@
+async function generateNews() {
+    const response = await fetch('./json/news.json');
+    const newsData = await response.json();
+    const newsContainer1 = document.getElementById('news1');
+    const newsContainer1 = document.getElementById('news2');
+    let nextColumn = newsContainer1
+    newsData.forEach(news => {
+        const newsElement = createNewsElement(news.title, news.content, news.image);
+        if (nextColumn == newsContainer1) {
+            let nextColumn = newsContainer2
+            newsContainer1.appendChild(newsElement);
+        } else if (nextColumn == newsContainer2) {
+            let nextColumn = newsContainer2
+            newsContainer1.appendChild(newsElement);
+        }
+        
+    });
+}
+
+  function createNewsElement(title, content, image) {
+    const element = document.createElement('div');
+    element.classList.add('news');
+
+    const imageElement = document.createElement('img')
+    imageElement.src = "/images/" + image;
+    imageElement.classList.add('news-image');
+
+
+    const titleElement = document.createElement('h2');
+    titleElement.textContent = title;
+    titleElement.classList.add('news-title');
+
+    const contentElement = document.createElement('span');
+    contentElement.innerHTML = content;
+    contentElement.classList.add('news-content');
+
+    element.appendChild(imageElement);
+    element.appendChild(titleElement);
+    element.appendChild(contentElement);
+    return element;
+  }
